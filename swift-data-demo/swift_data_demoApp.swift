3f96dd7c070b1b47d6939afcbb153b196caf367a
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-03 13:42:09 -0800

    Initial Commit

---
+++ swift-data-demo/swift_data_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_demoApp.swift
+//  swift-data-demo
+//
+//  Created by Amir Kabiri on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
